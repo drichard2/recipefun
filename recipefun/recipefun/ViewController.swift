b47f99ecd220758681d46df304111e4a05998e63
Daniel Richard <daniel.richard@outlook.com>
2018-11-27 18:22:11 -0800

    f
    df

---
+++ recipefun/recipefun/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  recipefun
+//
+//  Created by Daniel Richard on 11/27/18.
+//  Copyright © 2018 Daniel Richard. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+
+class ViewController: UIViewController, WKNavigationDelegate{
+    
+    var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        
+        // 1
+        let url = URL(string: "https://www.youtube.com/watch?v=yEcUlJO-pZ4")!
+        webView.load(URLRequest(url: url))
+        
+        // 2
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
+    }
+    
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+}
+
