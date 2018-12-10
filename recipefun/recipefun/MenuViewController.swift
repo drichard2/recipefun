3c3f7a18c07205beec23b7ba394cb376e875ee85
Daniel Richard <daniel.richard@outlook.com>
2018-12-09 20:48:32 -0800

    dfgf
    gfd

--- recipefun/recipefun/MenuViewController.swift
+++ recipefun/recipefun/MenuViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate  {
     
 
 
